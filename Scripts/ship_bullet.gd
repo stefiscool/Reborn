84477eba0d81420bc73c546872583e65a3fd620d
stefiscool <149964402+stefiscool@users.noreply.github.com>
2025-06-19 14:39:26 -0400

    Update ship_bullet.gd

--- Scripts/ship_bullet.gd
+++ Scripts/ship_bullet.gd
@@ -6,6 +6,22 @@ extends Area2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y -= 1000 * delta
+	match Global.shipweapon:
+		"Ion Cannons":
+			$Sprite2D.frame = 0
+		"Precision Lasers":
+			$Sprite2D.frame = 7
+		"Heavy Lasers":
+			$Sprite2D.frame = 4
+		"Heavy Cannons":
+			$Sprite2D.frame = 5
+		"Kinetic Cannons":
+			$Sprite2D.frame = 2
+		"Rapid Blasters":
+			$Sprite2D.frame = 1
+		"Plasma Cannons:":
+			$Sprite2D.frame = 3
+		
 
 
 func _on_body_entered(body: Node2D) -> void:
